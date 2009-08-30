6b040447e779da72dc9e7fb83ebec33329b366db
Jeremy Knope <jerome@buttered-cat.com>
2009-08-30 14:36:33 -0400

    Removed old readme and fixed urls in stats page

--- -/pages/stats.php
+++ -/pages/stats.php
@@ -12,7 +12,7 @@
 <? foreach($top_urls as $url) { ?>
 <tr>
 	<td><?=$url['url']?></td>
-	<td><?=LESSN_URL.base_convert($url['id'], 10, 36);?></td>
+	<td><?=BCURLS_URL.base_convert($url['id'], 10, 36);?></td>
 	<td><?=$url['hits']?></td>
 </tr>
 <? } ?>
