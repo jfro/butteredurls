70cf4ab3710496b052b554f9058615fdacb9904f
Alan Hogan <contact@alanhogan.com>
2010-02-22 02:16:51 -0700

    The 005 migration now adds default auto-increment (base 10) values for slug types of 0. These rows need to exist to automatically assign new slugs properly.

--- -/migrations/005_create_autoslug_table.php
+++ -/migrations/005_create_autoslug_table.php
@@ -8,6 +8,8 @@ class Create_Autoslug_Table extends Migration
 {
 	function up()
 	{
+		$prefix = DB_PREFIX;
+		
 		$t = $this->createTable(DB_PREFIX.'autoslug');
 		$t->column('id', 'serial', array('primary_key' => true, 'null' => false));
 		$t->column('method', 'string', array('null' => false, 'size' =>'31'));
@@ -15,6 +17,20 @@ class Create_Autoslug_Table extends Migration
 		$t->save();
 
 		$this->createIndex(DB_PREFIX.'autoslug', 'method', 'method_index', 'unique');
+		
+		// Populate
+		$add_methods_sql = <<<EOSQL
+		INSERT INTO  ${prefix}autoslug (id ,method ,base10)
+		VALUES (NULL ,  'base36',  '0'), 
+		(NULL ,  'base62',  '0'), 
+		(NULL ,  'mixed-smart',  '0'), 
+		(NULL ,  'lower-smart',  '0');		
+EOSQL;
+		
+		$ins = $this->db->prepare($add_methods_sql);
+		if( ! $ins->execute()){
+			throw new Exception('Failed to add rows for autoslug methods.');
+		}
 
 	}
 	
