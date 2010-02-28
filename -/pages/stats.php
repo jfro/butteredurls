2b943789e66094c59257bc2b23f77f236a45883e
Alan Hogan <contact@alanhogan.com>
2010-02-27 21:06:13 -0700

    Fixed issue #2, stats table.

--- -/pages/stats.php
+++ -/pages/stats.php
@@ -9,13 +9,15 @@
 		<th>Hits</th>
 	</tr>
 	
-<? foreach($top_urls as $url) { ?>
-<tr>
-	<td><?=$url['url']?></td>
-	<td><?=BCURLS_URL.base_convert($url['id'], 10, 36);?></td>
-	<td><?=$url['hits']?></td>
-</tr>
-<? } ?>
+<?php foreach($top_urls as $url) { 
+	$short = htmlspecialchars(BCURLS_URL.$url['custom_url'], ENT_QUOTES, 'UTF-8');
+	?>
+	<tr>
+		<td><?php echo htmlspecialchars($url['url'], ENT_QUOTES, 'UTF-8')?></td>
+		<td><!-- <a href="<?php /*=$short*/ ?>"> --><?php echo $short?><!-- </a> --></td>
+		<td><?php echo $url['hits']?></td>
+	</tr>
+<?php } ?>
 
 </table>
 
@@ -25,12 +27,12 @@
 		<th>Hits</th>
 	</tr>
 	
-<? foreach($top_referers as $url) { ?>
+<?php foreach($top_referers as $url) { ?>
 <tr>
-	<td><?=$url['referer']?></td>
-	<td><?=$url['hits']?></td>
+	<td><?php echo htmlspecialchars($url['referer'], ENT_QUOTES, 'UTF-8')?></td>
+	<td><?php echo $url['hits']?></td>
 </tr>
-<? } ?>
+<?php } ?>
 
 </table>
 
